82d61f3cdc3b116720e45b6bbeb0b1118bf72427
André Yamamoto <aayandre94@gmail.com>
2017-05-20 10:21:28 -0300

    Jogo umas alterações

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -19,15 +19,15 @@ public class Jogo {
 
         //Entrada
         System.out.println("Personagens: ");
-        
+
         Cores.cor("verde");
         System.out.println("Darwin:");
         System.out.println("(Características)\n");
-        
+
         Cores.cor("azul");
         System.out.println("Henry:");
         System.out.println("(Características)\n");
-        
+
         Cores.cor("vermelho");
         System.out.println("Lala:");
         System.out.println("(Características)\n");
@@ -35,27 +35,36 @@ public class Jogo {
         System.out.println("Selecione um personagem: ");
         String personagem = ler.next();
 
-        //Encontrando o personagem e validando-o
-        while (!"darwin".equalsIgnoreCase(personagem.replaceAll(" ", ""))
-                && !"henry".equalsIgnoreCase(personagem.replaceAll(" ", ""))
-                && !"lala".equalsIgnoreCase(personagem.replaceAll(" ", ""))) {
-            
-            //Revalidação do personagem
-            System.out.println("Escolha um dos personagens.");
-            personagem = ler.next();
-        }//Encontrando o personagem e validando-o -- FIM
-        
+//        //Encontrando o personagem e validando-o
+//        while (!"darwin".equalsIgnoreCase(personagem.replaceAll(" ", ""))
+//                && !"henry".equalsIgnoreCase(personagem.replaceAll(" ", ""))
+//                && !"lala".equalsIgnoreCase(personagem.replaceAll(" ", ""))) {
+//
+//            //Revalidação do personagem
+//            System.out.println("Escolha um dos personagens.");
+//            personagem = ler.next();
+//        }//Encontrando o personagem e validando-o -- FIM
+
         //Checando qual será o personagem/história usada
-        switch(personagem.toLowerCase()) {
+        switch (personagem.toLowerCase()) {
             case "darwin":
                 //Colocarei a classe da história do personagem
+                System.out.println("Darwin escolhido");
+                break;
             case "henry":
                 //Colocarei a classe da história do personagem
-            case"lala":
+                System.out.println("Henry escolhido");
+                break;
+            case "lala":
                 //Colocarei a classe da história do personagem
-                
+                System.out.println("Lala escolhida");
+                break;
+            default:
+                System.out.println("Escolha um dos personagens.");
+                personagem = ler.next();
+            //Quando o jogador terminar uma das histórias dar a escolha de ele fazer uma das que sobrou OU sair
+
         }//Checando qual será o personagem/história usada -- FIM
-        
-        
+
     }
 }
