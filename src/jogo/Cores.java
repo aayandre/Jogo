48dbf568c90735987d71bf5851aeb35e0e653663
André Yamamoto <aayandre94@gmail.com>
2017-05-28 16:00:48 -0300

    removi o main das cores

--- src/jogo/Cores.java
+++ src/jogo/Cores.java
@@ -10,50 +10,6 @@ package jogo;
  * @author andre.ayamamoto
  */
 public class Cores {
-    
-    public static void main(String[] args) {
-        cor("vermelho");
-        System.out.println("Este texto é vermelho...");
-        cor("verde");
-        System.out.println("Este texto é verde...");
-        cor("azul");
-        System.out.println("Este texto é azul...");
-        negrito(true);
-        System.out.println("Este texto é azul diferente...");
-        negrito(false);
-        System.out.println("Este texto é azul...");
-        cor("preto");
-        System.out.print("Que ");
-        cor("ciano");
-        System.out.print("cor");
-        cor("preto");
-        System.out.println(" é essa?");
-
-        System.out.println();
-        System.out.println("Agora, com fundo amarelo:");
-        fundo("amarelo");
-
-        cor("vermelho");
-        System.out.println("Este texto é vermelho...");
-        cor("verde");
-        System.out.println("Este texto é verde...");
-        cor("azul");
-        System.out.println("Este texto é azul...");
-        negrito(true);
-        System.out.println("Este texto é azul diferente...");
-        negrito(false);
-        System.out.println("Este texto é azul...");
-        cor("preto");
-        System.out.print("Que ");
-        cor("ciano");
-        System.out.print("cor");
-        cor("preto");
-        System.out.println(" é essa?");
-
-        reset();
-        System.out.println();
-        System.out.println("De volta ao \"normal\"...");
-    }
 
     public static void reset() {
         System.out.printf("\u001b[0m");
@@ -136,5 +92,5 @@ public class Cores {
 
         System.out.printf("\u001b[%dm", valor);
     }
-    
+
 }
